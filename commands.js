4cc53226c82886adc40ff23ac3f715b55aaa0aac
Pratik Verma <40285992+rexyagami@users.noreply.github.com>
2023-11-09 03:05:10 +0530

    modified-command-command allowed everywhere

--- commands.js
+++ commands.js
@@ -65,11 +65,8 @@ const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
     console.log("Started refreshing application (/) commands.");
 
     await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.APPLICATION_ID,
-        process.env.GUILD_ID
-      ),
-      { body: commands }
+    Routes.applicationCommands(process.env.APPLICATION_ID),
+    { body: commands },
     );
 
     console.log("Successfully reloaded application (/) commands.");
